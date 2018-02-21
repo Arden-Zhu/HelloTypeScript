f9e3886b4bac894699fbc602e5cc114babab95a3
Arden Zhu <arden.zhu@lafayette148.com>
2018-02-21 14:51:08 -0500

    classes

--- HelloTypeScript/Classes.js
+++ HelloTypeScript/Classes.js
@@ -1,3 +1,18 @@
+var FirstNameSpace;
+(function (FirstNameSpace) {
+    var NotExported = /** @class */ (function () {
+        function NotExported() {
+        }
+        return NotExported;
+    }());
+    var NameSpaceClass = /** @class */ (function () {
+        function NameSpaceClass() {
+        }
+        return NameSpaceClass;
+    }());
+    FirstNameSpace.NameSpaceClass = NameSpaceClass;
+})(FirstNameSpace || (FirstNameSpace = {}));
+;
 describe("Classes.ts", function () {
     it("Classes & Class Properties", function () {
         var Simple = /** @class */ (function () {
@@ -47,22 +62,170 @@ describe("Classes.ts", function () {
         expect(simpleB.other()).toBe(10);
     });
     it("Constructor", function () {
+        var Simple = /** @class */ (function () {
+            function Simple(_id) {
+                this.id = _id;
+            }
+            Simple.prototype.getId = function () {
+                return this.id;
+            };
+            return Simple;
+        }());
+        var simple = new Simple(100);
+        expect(simple.getId()).toBe(100);
     });
-    it("Class functions", function () {
-    });
-    it("Interface function definitions", function () {
+    it("Class functions & Interface function definitions", function () {
+        var ComplexType = /** @class */ (function () {
+            function ComplexType(idArg, nameArg) {
+                this.id = idArg;
+                this.name = nameArg;
+            }
+            ComplexType.prototype.print = function () {
+                return "id:" + this.id + " name:" + this.name;
+            };
+            ComplexType.prototype.usingTheAnyKeyword = function (arg1) {
+                this.id = arg1;
+            };
+            ComplexType.prototype.usingOptionalParameters = function (optionalArg1) {
+                if (optionalArg1) {
+                    this.id = optionalArg1;
+                }
+            };
+            ComplexType.prototype.usingDefaultParameters = function (defaultArg1) {
+                if (defaultArg1 === void 0) { defaultArg1 = 0; }
+                this.id = defaultArg1;
+            };
+            ComplexType.prototype.usingRestSyntax = function () {
+                var argArray = [];
+                for (var _i = 0; _i < arguments.length; _i++) {
+                    argArray[_i] = arguments[_i];
+                }
+                if (argArray.length > 0) {
+                    this.id = argArray[0];
+                }
+            };
+            ComplexType.prototype.usingFunctionCallbacks = function (callback) {
+                callback(this.id);
+            };
+            return ComplexType;
+        }());
+        var c = new ComplexType(1, "a");
+        expect(c.print()).toBe("id:1 name:a");
+        c = new ComplexType("2", "b");
+        expect(c.print()).toBe("id:2 name:b");
+        /*
+            it shows that type checking is only running on compile time,
+            NOT on running time, which making it completely different from C#
+            So, I think, it is MUST to have type guard in overload functions/constructor
+        */
+        c = new ComplexType("c", "c");
+        expect(c.print()).toBe("id:c name:c");
     });
     it("Class modifiers", function () {
+        var ClassWithPrivateProperty = /** @class */ (function () {
+            function ClassWithPrivateProperty(_id, _name) {
+                this.id = _id;
+                this.name = _name;
+            }
+            ClassWithPrivateProperty.prototype.getId = function () {
+                return this.id;
+            };
+            return ClassWithPrivateProperty;
+        }());
+        var privateAccess = new ClassWithPrivateProperty(10, "test");
+        privateAccess.name = "overrided";
+        // privateAccess.id = 20;  // it NOT passes compiling
+        expect(privateAccess.getId()).toBe(10);
+        expect(privateAccess.name).toBe("overrided");
     });
     it("Constructor access modifiers", function () {
+        var ClassWithPrivateProperty = /** @class */ (function () {
+            // constrcutor access modifiers are handy, but some developer may NOT like it
+            function ClassWithPrivateProperty(id, name) {
+                this.id = id;
+                this.name = name;
+            }
+            ClassWithPrivateProperty.prototype.getId = function () {
+                return this.id;
+            };
+            return ClassWithPrivateProperty;
+        }());
+        var privateAccess = new ClassWithPrivateProperty(10, "test");
+        privateAccess.name = "overrided";
+        // privateAccess.id = 20;  // it NOT passes compiling
+        expect(privateAccess.getId()).toBe(10);
+        expect(privateAccess.name).toBe("overrided");
     });
     it("Readonly Properties", function () {
+        var ClassWithReadOnly = /** @class */ (function () {
+            //readonly name: string;
+            function ClassWithReadOnly(name) {
+                this.name = name;
+                //this.name = _name;
+            }
+            ClassWithReadOnly.prototype.getName = function () {
+                return this.name;
+            };
+            ;
+            ClassWithReadOnly.prototype.setReadOnly = function (_name) {
+                //this.name = _name; // generates a compile error 
+            };
+            ;
+            return ClassWithReadOnly;
+        }());
+        var c = new ClassWithReadOnly("test");
+        expect(c.getName()).toBe("test");
     });
     it("Class Property accessors", function () {
+        // supported only after ECMAScript 5
+        var ClassWithAccessors = /** @class */ (function () {
+            function ClassWithAccessors() {
+            }
+            Object.defineProperty(ClassWithAccessors.prototype, "id", {
+                get: function () {
+                    console.log("inside get id()");
+                    return this._id;
+                },
+                set: function (value) {
+                    console.log("inside set id()");
+                    this._id = value;
+                },
+                enumerable: true,
+                configurable: true
+            });
+            return ClassWithAccessors;
+        }());
+        var c = new ClassWithAccessors();
+        c.id = 2;
+        expect(c.id).toBe(2);
     });
     it("Static functions/properties", function () {
+        var Simple = /** @class */ (function () {
+            function Simple() {
+            }
+            Simple.getId = function () {
+                return this.id;
+            };
+            Simple.prototype.getStaticId = function () {
+                return Simple.id;
+            };
+            return Simple;
+        }());
+        Simple.id = 100;
+        expect(Simple.getId()).toBe(100);
+        expect(new Simple().getStaticId()).toBe(100);
     });
     it("Namespaces", function () {
+        //namespace FirstNameSpace { // it is NOT allowed to write namespace inside function
+        //    class NotExported {
+        //    }
+        //    export class NameSpaceClass {
+        //        id: number;
+        //    }
+        //} 
+        var firstNameSpace = new FirstNameSpace.NameSpaceClass();
+        //let notExported = new FirstNameSpace.NotExported(); // it NOT passed compiling
+        expect(1).toBe(1);
     });
 });
 //# sourceMappingURL=Classes.js.map
--- HelloTypeScript/Classes.ts
+++ HelloTypeScript/Classes.ts
@@ -1,4 +1,12 @@
-﻿describe("Classes.ts", () => {
+﻿namespace FirstNameSpace {
+    class NotExported {
+    }
+    export class NameSpaceClass {
+        id: number;
+    }
+};
+
+describe("Classes.ts", () => {
     it("Classes & Class Properties", () => {
         class Simple {
             id: number;
@@ -49,30 +57,187 @@
     });
 
     it("Constructor", () => {
+        class Simple {
+            constructor(_id: number) {
+                this.id = _id;
+            }
+            //constructor() { // NOT allowed multiple constructors
+            //    this.id = 10;
+            //}
+            id: number;
+            getId(): number {
+                return this.id;
+            }
+        }
+
+        let simple = new Simple(100);
+        expect(simple.getId()).toBe(100);
     });
 
-    it("Class functions", () => {
-    });
+    it("Class functions & Interface function definitions", () => {
+        interface IComplexType {
+            id: number;
+            name: string;
+            print(): string;
+            usingTheAnyKeyword(arg1: any): any;
+            usingOptionalParameters(optionalArg1?: number);
+            usingDefaultParameters(defaultArg1?: number);
+            usingRestSyntax(...argArray: number[]);
+            usingFunctionCallbacks(callback: (id: number) => string);
+        } 
 
-    it("Interface function definitions", () => {
+        class ComplexType implements IComplexType {
+            id: number;
+            name: string;
+            constructor(idArg: number, nameArg: string);
+            constructor(idArg: string, nameArg: string);
+            constructor(idArg: any, nameArg: any) {
+                this.id = idArg;
+                this.name = nameArg;
+            }
+            print(): string {
+                return "id:" + this.id + " name:" + this.name;
+            }
+            usingTheAnyKeyword(arg1: any): any {
+                this.id = arg1;
+            }
+            usingOptionalParameters(optionalArg1?: number) {
+                if (optionalArg1) {
+                    this.id = optionalArg1;
+                }
+            }
+            usingDefaultParameters(defaultArg1: number = 0) {
+                this.id = defaultArg1;
+            }
+            usingRestSyntax(...argArray: number[]) {
+                if (argArray.length > 0) {
+                    this.id = argArray[0];
+                }
+            }
+            usingFunctionCallbacks(callback: (id: number) => string) {
+                callback(this.id);
+            }
+        } 
+
+        let c = new ComplexType(1, "a");
+        expect(c.print()).toBe("id:1 name:a");
+
+        c = new ComplexType("2", "b");
+        expect(c.print()).toBe("id:2 name:b");
+
+        /*
+            it shows that type checking is only running on compile time,
+            NOT on running time, which making it completely different from C#
+            So, I think, it is MUST to have type guard in overload functions/constructor
+        */
+        c = new ComplexType("c", "c");
+        expect(c.print()).toBe("id:c name:c"); 
     });
 
     it("Class modifiers", () => {
+        class ClassWithPrivateProperty {
+            private id: number;
+            // by default , class properties/functions are public
+            name: string;
+            getId(): number {
+                return this.id;
+            }
+            constructor(_id: number, _name: string) {
+                this.id = _id;
+                this.name = _name;
+            }
+        }
+
+        let privateAccess = new ClassWithPrivateProperty(10, "test");
+        privateAccess.name = "overrided";
+        // privateAccess.id = 20;  // it NOT passes compiling
+        expect(privateAccess.getId()).toBe(10);
+        expect(privateAccess.name).toBe("overrided");
     });
 
     it("Constructor access modifiers", () => {
+        class ClassWithPrivateProperty {
+            // constrcutor access modifiers are handy, but some developer may NOT like it
+            constructor(private id: number, public name: string) {
+            }
+            getId(): number {
+                return this.id;
+            }
+        }
+
+        let privateAccess = new ClassWithPrivateProperty(10, "test");
+        privateAccess.name = "overrided";
+        // privateAccess.id = 20;  // it NOT passes compiling
+        expect(privateAccess.getId()).toBe(10);
+        expect(privateAccess.name).toBe("overrided");
     });
 
     it("Readonly Properties", () => {
+        class ClassWithReadOnly {
+            //readonly name: string;
+            constructor(readonly name: string) {
+                //this.name = _name;
+            }
+            getName(): string {
+                return this.name;
+            };
+            setReadOnly(_name: string) {
+                
+                //this.name = _name; // generates a compile error 
+            };
+        }
+
+        let c = new ClassWithReadOnly("test");
+        expect(c.getName()).toBe("test");
     });
 
     it("Class Property accessors", () => {
+        // supported only after ECMAScript 5
+        class ClassWithAccessors {
+            private _id: number;
+            get id() {
+                console.log(`inside get id()`);
+                return this._id;
+            }
+            set id(value: number) {
+                console.log(`inside set id()`);
+                this._id = value;
+            }
+        } 
+
+        let c = new ClassWithAccessors();
+        c.id = 2;
+        expect(c.id).toBe(2);
     });
 
     it("Static functions/properties", () => {
+        class Simple {
+            static id: number;
+            static getId(): number {
+                return this.id;
+            }
+
+            getStaticId(): number {
+                return Simple.id;
+            }
+        }
+
+        Simple.id = 100;
+        expect(Simple.getId()).toBe(100);
+        expect(new Simple().getStaticId()).toBe(100);
     });
 
     it("Namespaces", () => {
+        //namespace FirstNameSpace { // it is NOT allowed to write namespace inside function
+        //    class NotExported {
+        //    }
+        //    export class NameSpaceClass {
+        //        id: number;
+        //    }
+        //} 
+        let firstNameSpace = new FirstNameSpace.NameSpaceClass();
+        //let notExported = new FirstNameSpace.NotExported(); // it NOT passed compiling
+        expect(1).toBe(1);
     });
 
 });
