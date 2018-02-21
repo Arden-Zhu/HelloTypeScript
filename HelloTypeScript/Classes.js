b9c61cf057d019e42b1a61afc7a3f39fd438c6e2
Arden Zhu <arden.zhu@lafayette148.com>
2018-02-21 14:16:54 -0500

    classes

--- HelloTypeScript/Classes.js
+++ HelloTypeScript/Classes.js
@@ -1,7 +1,68 @@
-describe("Classes.ts ", function () {
-    it("should passed", function () {
-        var undefinedValue = "test6";
-        expect(undefinedValue).toBe("test6");
+describe("Classes.ts", function () {
+    it("Classes & Class Properties", function () {
+        var Simple = /** @class */ (function () {
+            function Simple() {
+            }
+            Simple.prototype.getId = function () {
+                return this.id;
+            };
+            return Simple;
+        }());
+        var simple = new Simple();
+        simple.id = 100;
+        expect(simple.getId()).toBe(100);
+    });
+    it("Implementing Interfaces", function () {
+        ;
+        var SimpleA = /** @class */ (function () {
+            function SimpleA() {
+            }
+            SimpleA.prototype.getClassName = function () {
+                return "SimpleA";
+            };
+            ;
+            SimpleA.prototype.newFunction = function () {
+                // it shows that class can have new function against interface
+            };
+            ;
+            return SimpleA;
+        }());
+        var SimpleB = /** @class */ (function () {
+            function SimpleB() {
+            }
+            SimpleB.prototype.getClassName = function () {
+                return "SimpleB";
+            };
+            ;
+            SimpleB.prototype.other = function () {
+                return 10;
+            };
+            return SimpleB;
+        }());
+        var simple = new SimpleA();
+        expect(simple.getClassName()).toBe("SimpleA");
+        simple = new SimpleB();
+        expect(simple.getClassName()).toBe("SimpleB");
+        var simpleB = simple; // type cast
+        expect(simpleB.other()).toBe(10);
+    });
+    it("Constructor", function () {
+    });
+    it("Class functions", function () {
+    });
+    it("Interface function definitions", function () {
+    });
+    it("Class modifiers", function () {
+    });
+    it("Constructor access modifiers", function () {
+    });
+    it("Readonly Properties", function () {
+    });
+    it("Class Property accessors", function () {
+    });
+    it("Static functions/properties", function () {
+    });
+    it("Namespaces", function () {
     });
 });
 //# sourceMappingURL=Classes.js.map
--- HelloTypeScript/Classes.ts
+++ HelloTypeScript/Classes.ts
@@ -1,7 +1,79 @@
-﻿describe("Classes.ts ", () => {
-    it("should passed", () => {
-        let undefinedValue = "test6";
-        expect(undefinedValue).toBe("test6");
+﻿describe("Classes.ts", () => {
+    it("Classes & Class Properties", () => {
+        class Simple {
+            id: number;
+            getId(): number {
+                return this.id;
+            }
+        }
+
+        let simple = new Simple();
+        simple.id = 100;
+        expect(simple.getId()).toBe(100);
     });
+
+    it("Implementing Interfaces", () => {
+        interface ISimple {
+            getClassName(): string;
+        };
+
+        interface IOther {
+            other(): number;
+        }
+
+        class SimpleA implements ISimple {
+            getClassName(): string {
+                return "SimpleA";
+            };
+            newFunction(): void {
+                // it shows that class can have new function against interface
+            };
+        }
+
+        class SimpleB implements ISimple, IOther { // multiple interface implementation is OK
+            getClassName(): string {
+                return "SimpleB";
+            };
+            other(): number {
+                return 10;
+            }
+        }
+
+
+        let simple: ISimple = new SimpleA();
+        expect(simple.getClassName()).toBe("SimpleA");
+        simple = new SimpleB();
+        expect(simple.getClassName()).toBe("SimpleB");
+        let simpleB: SimpleB = simple as SimpleB; // type cast
+        expect(simpleB.other()).toBe(10);
+    });
+
+    it("Constructor", () => {
+    });
+
+    it("Class functions", () => {
+    });
+
+    it("Interface function definitions", () => {
+    });
+
+    it("Class modifiers", () => {
+    });
+
+    it("Constructor access modifiers", () => {
+    });
+
+    it("Readonly Properties", () => {
+    });
+
+    it("Class Property accessors", () => {
+    });
+
+    it("Static functions/properties", () => {
+    });
+
+    it("Namespaces", () => {
+    });
+
 });
 
