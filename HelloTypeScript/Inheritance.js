6e5393eb7067bd9987a74618dc2023ff2229833c
Arden Zhu <arden.zhu@lafayette148.com>
2018-02-23 13:58:38 -0500

    Inheritance

--- HelloTypeScript/Inheritance.js
+++ HelloTypeScript/Inheritance.js
@@ -1,7 +1,133 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 describe("Inheritance.ts", function () {
-    it("should passed", function () {
-        var undefinedValue = "test6";
-        expect(undefinedValue).toBe("test6");
+    it("Interface inheritance", function () {
+        var InterfaceInheritanceClass = /** @class */ (function () {
+            function InterfaceInheritanceClass() {
+            }
+            return InterfaceInheritanceClass;
+        }());
+        var c = new InterfaceInheritanceClass();
+        expect(c.id).toBe(undefined);
+        expect(c.name).toBe(undefined);
+    });
+    it("Class inheritance", function () {
+        var BaseClass = /** @class */ (function () {
+            function BaseClass() {
+            }
+            return BaseClass;
+        }());
+        var DerivedFromBaseClass = /** @class */ (function (_super) {
+            __extends(DerivedFromBaseClass, _super);
+            function DerivedFromBaseClass() {
+                return _super !== null && _super.apply(this, arguments) || this;
+            }
+            return DerivedFromBaseClass;
+        }(BaseClass));
+        var c = new DerivedFromBaseClass();
+        expect(c.id).toBe(undefined);
+        expect(c.name).toBe(undefined);
+    });
+    it("The super keyword & function overloading", function () {
+        var BaseClassWithConstructor = /** @class */ (function () {
+            function BaseClassWithConstructor(_id) {
+                this.id = _id;
+            }
+            BaseClassWithConstructor.prototype.getDump = function () {
+                return "id = " + this.id + ";";
+            };
+            return BaseClassWithConstructor;
+        }());
+        var DerivedClassWithConstructor = /** @class */ (function (_super) {
+            __extends(DerivedClassWithConstructor, _super);
+            function DerivedClassWithConstructor(_id, _name) {
+                var _this = _super.call(this, _id) || this;
+                _this.name = _name;
+                return _this;
+            }
+            DerivedClassWithConstructor.prototype.getDump = function () {
+                //return super() + `name = ${this.name};`; // super() can be only in constructor
+                return _super.prototype.getDump.call(this) + ("name = " + this.name + ";");
+            };
+            return DerivedClassWithConstructor;
+        }(BaseClassWithConstructor));
+        var c = new DerivedClassWithConstructor(1, 'a');
+        expect(c.getDump()).toBe("id = 1;name = a;");
+    });
+    it("Protected class members", function () {
+        var ClassUsingProtected = /** @class */ (function () {
+            function ClassUsingProtected(id) {
+                this.id = id;
+            }
+            return ClassUsingProtected;
+        }());
+        var DerivedFromProtected = /** @class */ (function (_super) {
+            __extends(DerivedFromProtected, _super);
+            // it shows that I can write in this way to declare properties and init them
+            // Note the public accessor, it is required to write here
+            function DerivedFromProtected(id, name) {
+                var _this = _super.call(this, id) || this;
+                _this.name = name;
+                return _this;
+            }
+            DerivedFromProtected.prototype.getId = function () {
+                return this.id;
+            };
+            return DerivedFromProtected;
+        }(ClassUsingProtected));
+        var c = new DerivedFromProtected(10, 'a');
+        expect(c.getId()).toBe(10);
+        expect(c.name).toBe("a");
+        // derivedFromProtected.id = 1; // it can not pass because id is a protected property
+    });
+    it("Abstract classes", function () {
+        var AbstractEmployee = /** @class */ (function () {
+            function AbstractEmployee() {
+            }
+            AbstractEmployee.prototype.printDetails = function () {
+                console.log(this.getDetails());
+            };
+            return AbstractEmployee;
+        }());
+        var NewEmployee = /** @class */ (function (_super) {
+            __extends(NewEmployee, _super);
+            function NewEmployee() {
+                return _super !== null && _super.apply(this, arguments) || this;
+            }
+            NewEmployee.prototype.getDetails = function () {
+                return "id : " + this.id + ", name : " + this.name;
+            };
+            return NewEmployee;
+        }(AbstractEmployee));
+        var NewManager = /** @class */ (function (_super) {
+            __extends(NewManager, _super);
+            function NewManager() {
+                return _super !== null && _super.apply(this, arguments) || this;
+            }
+            NewManager.prototype.getDetails = function () {
+                return _super.prototype.getDetails.call(this)
+                    + (", employeeCount " + this.Employees.length);
+            };
+            return NewManager;
+        }(NewEmployee));
+        var employee = new NewEmployee();
+        employee.id = 1;
+        employee.name = "Employee Name";
+        expect(employee.getDetails()).toBe("id : 1, name : Employee Name");
+        var manager = new NewManager();
+        manager.id = 2;
+        manager.name = "Manager Name";
+        manager.Employees = new Array();
+        // it shows polymorphic
+        expect(manager.getDetails()).toBe("id : 2, name : Manager Name, employeeCount 0");
     });
 });
 //# sourceMappingURL=Inheritance.js.map
--- HelloTypeScript/Inheritance.ts
+++ HelloTypeScript/Inheritance.ts
@@ -1,7 +1,139 @@
 ﻿describe("Inheritance.ts", () => {
-    it("should passed", () => {
-        let undefinedValue = "test6";
-        expect(undefinedValue).toBe("test6");
+    it("Interface inheritance", () => {
+        interface IBase {
+            id: number;
+        }
+
+        interface IDerivedFromBase extends IBase {
+            name: string;
+        }
+
+        class InterfaceInheritanceClass implements
+            IDerivedFromBase {
+            id: number;
+            name: string;
+        } 
+
+        let c = new InterfaceInheritanceClass();
+        expect(c.id).toBe(undefined);
+        expect(c.name).toBe(undefined);
+    });
+
+    it("Class inheritance", () => {
+        interface IBase {
+            id: number;
+        }
+
+        interface IDerivedFromBase extends IBase {
+            name: string;
+        }
+
+        class BaseClass implements IBase {
+            id: number;
+        }
+
+        class DerivedFromBaseClass extends BaseClass
+            implements IBase, IDerivedFromBase { // Notes that the class extends the base class and implements 2 interfaces
+            name: string;
+        } 
+
+        let c = new DerivedFromBaseClass();
+        expect(c.id).toBe(undefined);
+        expect(c.name).toBe(undefined);
+    });
+
+    it("The super keyword & function overloading", () => {
+        class BaseClassWithConstructor {
+            private id: number;
+            constructor(_id: number) {
+                this.id = _id;
+            }
+
+            getDump(): string {
+                return `id = ${this.id};`;
+            }
+        }
+
+        class DerivedClassWithConstructor extends
+            BaseClassWithConstructor {
+            private name: string;
+            constructor(_id: number, _name: string) {
+                super(_id);
+                this.name = _name;
+            }
+            getDump(): string {
+                //return super() + `name = ${this.name};`; // super() can be only in constructor
+                return super.getDump() + `name = ${this.name};`
+            }
+        } 
+
+        let c = new DerivedClassWithConstructor(1, 'a');
+        expect(c.getDump()).toBe("id = 1;name = a;");
+    });
+
+    it("Protected class members", () => {
+        class ClassUsingProtected {
+            constructor(protected id: number) {
+
+            }
+        }
+
+        class DerivedFromProtected extends
+            ClassUsingProtected {
+            // it shows that I can write in this way to declare properties and init them
+            // Note the public accessor, it is required to write here
+            constructor(id: number, public name: string) { 
+                super(id);
+            }
+
+            public getId() {
+                return this.id;
+            }
+        } 
+
+        var c = new DerivedFromProtected(10, 'a');
+        expect(c.getId()).toBe(10);
+        expect(c.name).toBe("a");
+        // derivedFromProtected.id = 1; // it can not pass because id is a protected property
+    });
+
+    it("Abstract classes", () => {
+        abstract class AbstractEmployee { // abstract class
+            public id: number;
+            public name: string;
+            abstract getDetails(): string; // abstract function
+            public printDetails() {
+                console.log(this.getDetails());
+            }
+        }
+
+        class NewEmployee extends AbstractEmployee {
+            getDetails(): string {
+                return `id : ${this.id}, name : ${this.name}`;
+            }
+        }
+
+        class NewManager extends NewEmployee {
+            public Employees: NewEmployee[];
+            getDetails(): string {
+                return super.getDetails()
+                    + `, employeeCount ${this.Employees.length}`;
+            }
+        } 
+
+        var employee = new NewEmployee();
+        employee.id = 1;
+        employee.name = "Employee Name";
+
+        expect(employee.getDetails()).toBe("id : 1, name : Employee Name");  
+
+        var manager = new NewManager();
+        manager.id = 2;
+        manager.name = "Manager Name";
+        manager.Employees = new Array();
+
+        // it shows polymorphic
+        expect(manager.getDetails()).toBe("id : 2, name : Manager Name, employeeCount 0"); 
     });
 });
 
