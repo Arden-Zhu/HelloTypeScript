bbfc8c5048d5dbb44593c15365390570b908ac30
Arden Zhu <arden.zhu@lafayette148.com>
2018-02-28 14:43:26 -0500

    Generics

--- HelloTypeScript/Generics.js
+++ HelloTypeScript/Generics.js
@@ -1,7 +1,86 @@
 describe("Generics.ts", function () {
-    it("should passed", function () {
-        var undefinedValue = "test6";
-        expect(undefinedValue).toBe("test6");
+    it("Generic syntax", function () {
+        var Concatenator = /** @class */ (function () {
+            function Concatenator() {
+            }
+            Concatenator.prototype.concatenateArray = function (inputArray) {
+                var returnString = "";
+                for (var i = 0; i < inputArray.length; i++) {
+                    if (i > 0)
+                        returnString += ",";
+                    returnString += inputArray[i].toString();
+                }
+                return returnString;
+            };
+            return Concatenator;
+        }());
+        var stringConcat = new Concatenator();
+        var numberConcat = new Concatenator();
+        var stringArray = ["first", "second", "third"];
+        expect(stringConcat.concatenateArray(stringArray)).toBe("first,second,third");
+        expect(numberConcat.concatenateArray([1, 2, 3])).toBe("1,2,3");
+    });
+    it("Constraining the type of T", function () {
+        var Base = /** @class */ (function () {
+            function Base(id) {
+                this.id = id;
+            }
+            return Base;
+        }());
+        // constraint the type of T
+        var Comparer = /** @class */ (function () {
+            function Comparer() {
+            }
+            Comparer.prototype.isEqual = function (a, b) {
+                return a.id === b.id;
+            };
+            return Comparer;
+        }());
+        var a = new Base(1);
+        var b = new Base(2);
+        // create with class
+        var comparer = new Comparer();
+        expect(comparer.isEqual(a, b)).toBe(false);
+        var c2 = new Comparer(); // create with IBase
+        expect(c2.isEqual(a, b)).toBe(false);
+    });
+    it("Creating new objects within generics", function () {
+        var FirstClass = /** @class */ (function () {
+            function FirstClass() {
+            }
+            return FirstClass;
+        }());
+        var SecondClass = /** @class */ (function () {
+            function SecondClass() {
+            }
+            return SecondClass;
+        }());
+        /* it does not work
+        class GenericCreator<T> {
+            create(): T {
+                return new T();
+            }
+        }
+        */
+        var GenericCreator = /** @class */ (function () {
+            function GenericCreator() {
+            }
+            GenericCreator.prototype.create = function (arg1) {
+                return new arg1();
+            };
+            return GenericCreator;
+        }());
+        var creator1 = new GenericCreator();
+        var firstClass = creator1.create(FirstClass);
+        var creator2 = new GenericCreator();
+        var secondClass = creator2.create(SecondClass);
+        expect(typeof (firstClass)).toBe("object");
+    });
+    it("", function () {
+    });
+    it("", function () {
+    });
+    it("", function () {
     });
 });
 //# sourceMappingURL=Generics.js.map
--- HelloTypeScript/Generics.ts
+++ HelloTypeScript/Generics.ts
@@ -1,7 +1,95 @@
 ﻿describe("Generics.ts", () => {
-    it("should passed", () => {
-        let undefinedValue = "test6";
-        expect(undefinedValue).toBe("test6");
+    it("Generic syntax", () => {
+        class Concatenator<T> {
+            concatenateArray(inputArray: Array<T>): string {
+                let returnString = "";
+
+                for (let i = 0; i < inputArray.length; i++) {
+                    if (i > 0)
+                        returnString += ",";
+                    returnString += inputArray[i].toString();
+                }
+                return returnString;
+            }
+        }
+
+        var stringConcat = new Concatenator<string>();
+        var numberConcat = new Concatenator<number>();
+
+        var stringArray: string[] = ["first", "second", "third"];
+
+        expect(stringConcat.concatenateArray(stringArray)).toBe("first,second,third");
+        expect(numberConcat.concatenateArray([1, 2, 3])).toBe("1,2,3");
     });
+
+    it("Constraining the type of T", () => {
+        interface IBase {
+            id: number;
+        }
+
+        class Base implements IBase {
+            constructor(public id: number) {
+
+            }
+        }
+
+        // constraint the type of T
+        class Comparer<T extends IBase> {
+            isEqual(a: T, b: T): boolean {
+                return a.id === b.id;
+            }
+        }
+
+        let a = new Base(1);
+        let b = new Base(2);
+        // create with class
+        let comparer = new Comparer<Base>();
+        
+        expect(comparer.isEqual(a, b)).toBe(false);
+
+        let c2 = new Comparer(); // create with IBase
+        expect(c2.isEqual(a, b)).toBe(false);
+    });
+
+    it("Creating new objects within generics", () => {
+        class FirstClass {
+            id: number;
+        }
+
+        class SecondClass {
+            name: string;
+        }
+
+        /* it does not work
+        class GenericCreator<T> {
+            create(): T {
+                return new T();
+            }
+        }
+        */
+        class GenericCreator<T> {
+            create(arg1: { new(): T }): T {
+                return new arg1();
+            }
+        }
+
+        var creator1 = new GenericCreator<FirstClass>();
+        var firstClass: FirstClass = creator1.create(FirstClass);
+
+        var creator2 = new GenericCreator<SecondClass>();
+        var secondClass: SecondClass = creator2.create(SecondClass); 
+
+        expect(typeof (firstClass)).toBe("object");
+    });
+
+    it("", () => {
+    });
+
+    it("", () => {
+    });
+
+    it("", () => {
+    });
+
 });
 
