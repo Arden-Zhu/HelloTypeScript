f10b45e172ab3ca9a961c2dbc4222dce2ae8a668
Arden Zhu <arden.zhu@lafayette148.com>
2018-04-02 18:03:18 -0400

    sth

--- HelloTypeScript/SimpleJasmineTests.ts
+++
@@ -1,7 +0,0 @@
-﻿describe("tests/SimpleJasmineTests.ts ", () => {
-    it("should passed", () => {
-        let undefinedValue="test6";
-        expect(undefinedValue).toBe("test6");
-    });
-});
-
