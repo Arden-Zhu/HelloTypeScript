467b77406262b7d43b56caed0822b3413e55ad8a
Arden Zhu <420395671@qq.com>
2018-04-02 21:52:53 -0400

    Dict

--- HelloTypeScript/Dict.js
+++ HelloTypeScript/Dict.js
@@ -52,15 +52,44 @@ var DictHelper = /** @class */ (function () {
     };
     return DictHelper;
 }());
+function toStr(d) {
+    var s = DictHelper.map(d, function (v) { return v.id + v.value; }).join('');
+    return s;
+}
 describe("Dict.ts", function () {
-    it("duck typing", function () {
-        var complex1 = { id: 1, name: "name" };
-        var complex2 = { name: "name", id: 1 };
-        expect(complex1).toEqual(complex2);
-        expect(complex1).not.toBe(complex2);
-        expect(complex1).not.toBe({ id: 1, name: "name" });
-        complex2 = complex1;
-        expect(complex1).toBe(complex2);
+    it("IValue to Dict", function () {
+        var a = [
+            { id: 'z', value: '3', },
+            { id: 'y', value: '4', },
+            { id: 'x', value: '2', },
+        ];
+        var d = DictHelper.toValues(a);
+        expect(toStr(d)).toBe('z3y4x2');
+        d = DictHelper.remove(d, 'y');
+        expect(toStr(d)).toBe('z3x2');
+        d.y = { id: 'y', value: '5' };
+        expect(toStr(d)).toBe('z3x2y5');
+        var d2 = DictHelper.copy(d);
+        expect(toStr(d2)).toBe('z3x2y5');
+        d2.y2 = { id: 'y2', value: '6' };
+        expect(toStr(d2)).toBe('z3x2y5y26');
+    });
+    it("IValue to Dict Sort 2", function () {
+        var a = [
+            { id: '1', value: '1', },
+            { id: '3', value: '3', },
+            { id: '2', value: '2', },
+        ];
+        var d = DictHelper.toValues(a);
+        expect(toStr(d)).toBe('112233');
+        //d = DictHelper.remove(d, 'y');
+        //expect(toStr(d)).toBe('z3x2');
+        //d.y = { id: 'y', value: '5' };
+        //expect(toStr(d)).toBe('z3x2y5');
+        //let d2 = DictHelper.copy(d);
+        //expect(toStr(d2)).toBe('z3x2y5');
+        //d2.y2 = { id: 'y2', value: '6' };
+        //expect(toStr(d2)).toBe('z3x2y5y26');
     });
 });
 //# sourceMappingURL=Dict.js.map
--- HelloTypeScript/Dict.ts
+++ HelloTypeScript/Dict.ts
@@ -56,14 +56,52 @@ class DictHelper {
     }
 }
 
+function toStr(d: Dict<IValue>): string {
+    let s = DictHelper.map(d, v => v.id + v.value).join('');
+    return s;
+}
+
 describe("Dict.ts", () => {
-    it("duck typing", () => {
-        var complex1 = { id: 1, name: "name" };
-        var complex2 = { name: "name", id: 1 };
-        expect(complex1).toEqual(complex2);
-        expect(complex1).not.toBe(complex2);
-        expect(complex1).not.toBe({ id: 1, name: "name" });
-        complex2 = complex1;
-        expect(complex1).toBe(complex2);
+    it("IValue to Dict", () => {
+        let a: IValue[] = [
+            { id: 'z', value: '3', },
+            { id: 'y', value: '4', },
+            { id: 'x', value: '2', },
+        ]
+
+        let d = DictHelper.toValues(a);
+        expect(toStr(d)).toBe('z3y4x2');
+        d = DictHelper.remove(d, 'y');
+        expect(toStr(d)).toBe('z3x2');
+        d.y = { id: 'y', value: '5' };
+        expect(toStr(d)).toBe('z3x2y5');
+
+        let d2 = DictHelper.copy(d);
+        expect(toStr(d2)).toBe('z3x2y5');
+
+        d2.y2 = { id: 'y2', value: '6' };
+        expect(toStr(d2)).toBe('z3x2y5y26');
     });
+
+    it("IValue to Dict Sort 2", () => {
+        let a: IValue[] = [
+            { id: '1', value: '1', },
+            { id: '3', value: '3', },
+            { id: '2', value: '2', },
+        ]
+
+        let d = DictHelper.toValues(a);
+        expect(toStr(d)).toBe('112233');
+        //d = DictHelper.remove(d, 'y');
+        //expect(toStr(d)).toBe('z3x2');
+        //d.y = { id: 'y', value: '5' };
+        //expect(toStr(d)).toBe('z3x2y5');
+
+        //let d2 = DictHelper.copy(d);
+        //expect(toStr(d2)).toBe('z3x2y5');
+
+        //d2.y2 = { id: 'y2', value: '6' };
+        //expect(toStr(d2)).toBe('z3x2y5y26');
+    });
+
 });
