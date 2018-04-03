c28213214b52eeb431dea4796f53959be9a1450c
Arden Zhu <420395671@qq.com>
2018-04-02 23:12:30 -0400

    before try

--- HelloTypeScript/Map.js
+++ HelloTypeScript/Map.js
@@ -1,3 +1,13 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 describe("Map.ts", function () {
     function toStr(m) {
         var s = '';
@@ -8,14 +18,24 @@ describe("Map.ts", function () {
     }
     it("Map sort", function () {
         var a = [
-            ['1', '1',],
-            ['3', '3',],
-            ['2', '2',],
-            ['2', '4',],
-            ['1', '5',],
+            [1, '1',],
+            [3, '3',],
+            [2, '2',],
+            [2, '4',],
+            [1, '5',],
         ];
         var d = new Map(a);
         expect(toStr(d)).toBe('153324');
+        //d['2'] = '6';
+        d.set(2, '6');
+        expect(toStr(d)).toBe('153326');
     });
+    var Dict = /** @class */ (function (_super) {
+        __extends(Dict, _super);
+        function Dict() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        return Dict;
+    }(Map));
 });
 //# sourceMappingURL=Map.js.map
--- HelloTypeScript/Map.ts
+++ HelloTypeScript/Map.ts
@@ -1,5 +1,5 @@
 ﻿describe("Map.ts", () => {
-    function toStr(m: Map<string, string>): string {
+    function toStr(m: Map<number, string>): string {
         let s: string = '';
 
         m.forEach((value, key) => {
@@ -11,16 +11,89 @@
     it("Map sort", () => {
 
         let a: any[] = [
-            ['1', '1', ],
-            ['3', '3',],
-            ['2', '2', ],
-            ['2', '4',],
-            ['1', '5',],
+            [1, '1', ],
+            [3, '3',],
+            [2, '2', ],
+            [2, '4',],
+            [1, '5',],
         ]
 
-        let d = new Map<string, string>(a);
-
+        let d = new Map<number, string>(a);
+        
         expect(toStr(d)).toBe('153324');
+        //d['2'] = '6';
+        d.set(2, '6');
+        
+        expect(toStr(d)).toBe('153326');
     });
+
+    class Dict<T> extends Map<number, T> {
+        map<R>(cb: (value: T, index: number, key: number, m: Dict<T>) => R): R[] {
+            let idx: number = 0;
+            let r: R[] = [];
+            this.forEach((value, key) => {
+                r.push(cb(value, idx++, key, this));
+            });
+            return r;
+        }
+
+        static fromArray<T, K extends keyof T>(arr: T[], key: K): Dict<T> {
+            let r = new Dict<T>();
+            for (let a of arr)
+                r.set(Number(a[key]), a);
+            return r;
+        }
+    }
+
+    interface IValue {
+        id: string
+        value: string;
+        pid?: string;
+    }
+
+    class DictHelper {
+
+        static toDict<T, K extends keyof T>(a: T[], key: K): Dict<T> {
+            let r: Dict<T> = {}
+            for (var t of a) {
+                r[t[key].toString()] = t;
+            }
+            return r;
+        }
+
+        static toValues(a: IValue[]): Dict<IValue> {
+            return this.toDict(a, "id");
+        }
+
+        static mapValues<R>(m: Dict<IValue>, cb: (value: string, id: string) => R): R[] {
+            return Object.keys(m).map((id) => cb(m[id].value, id));
+        }
+
+        // a shallow copy
+        static copy<T>(source: Dict<T>): Dict<T> {
+            let r: Dict<T> = {};
+            for (var k of Object.keys(source)) {
+                r[k] = source[k];
+            }
+            return r;
+        }
+
+        static update<T extends IValue>(source: Dict<T>, t: T): Dict<T> {
+            let r: Dict<T> = {}
+            for (var k of Object.keys(source)) {
+                r[k] = (k !== t.id) ? source[k] : t;
+            }
+            return r;
+        }
+
+        static remove<T extends IValue>(source: Dict<T>, id: string): Dict<T> {
+            let r: Dict<T> = {}
+            for (var k of Object.keys(source)) {
+                if (k !== id)
+                    r[k] = source[k];
+            }
+            return r;
+        }
+    }
 
 });
